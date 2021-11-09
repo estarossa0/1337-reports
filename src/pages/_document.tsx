1ac6ea57a01648c1ba5919b162fc76beba6ba497
esta-rossa <rajiabdou9@gmail.com>
2021-11-09 18:24:59 +0100

    Initial commit from Create Next App

---
+++ src/pages/_document.tsx
@@ -0,0 +1,17 @@
+import { ColorModeScript } from '@chakra-ui/color-mode';
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class Document extends NextDocument {
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <ColorModeScript />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+import { Box, Text } from '@chakra-ui/react';
+
+const Index = () => <Text textColor="black">1337-reports</Text>;
+
+export default Index;

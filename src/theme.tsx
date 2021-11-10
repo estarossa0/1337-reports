9a7f376f31a6d3b1305893f2435ced299b3da5d0
esta-rossa <rajiabdou9@gmail.com>
2021-11-10 11:06:09 +0100

    feat(chakra): update theme config for chakra

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,21 +1,43 @@
-import { extendTheme } from '@chakra-ui/react'
-import { createBreakpoints } from '@chakra-ui/theme-tools'
+import { extendTheme } from '@chakra-ui/react';
+import { createBreakpoints } from '@chakra-ui/theme-tools';
 
-const fonts = { mono: `'Menlo', monospace` }
+const noneVariant = {
+  none: {
+    _focus: {
+      outline: 'none',
+      boxShadow: 'none',
+      colorScheme: 'none',
+    },
+  },
+};
 
 const breakpoints = createBreakpoints({
-  sm: '40em',
-  md: '52em',
-  lg: '64em',
-  xl: '80em',
-})
+  sm: '320px',
+  md: '768px',
+  lg: '960px',
+  xl: '1200px',
+  '2xl': '1600px',
+});
 
 const theme = extendTheme({
   colors: {
     black: '#16161D',
   },
-  fonts,
+  components: {
+    Button: {
+      variants: noneVariant,
+      defaultProps: {
+        variant: 'none',
+      },
+    },
+    Link: {
+      variants: noneVariant,
+      defaultProps: {
+        variant: 'none',
+      },
+    },
+  },
   breakpoints,
-})
+});
 
-export default theme
+export default theme;

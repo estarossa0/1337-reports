089cd711adb9e8f5676e48aff96a360d9a979bea
esta-rossa <rajiabdou9@gmail.com>
2021-12-15 19:27:52 +0100

    feat: set default `onError` for queries

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,10 +6,18 @@ import ParticlesBackground from "../components/particles-background";
 import UserModal from "../components/user-modal";
 import "../styles/Submit.css";
 import { QueryClient, QueryClientProvider, Hydrate } from "react-query";
+import router from "next/router";
+import { AxiosError } from "axios";
 
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
+  queryClient.setDefaultOptions({
+    queries: {
+      onError: (error: AxiosError) =>
+        router.replace(`/error?error=${error.message}`),
+    },
+  });
   return (
     <SessionProvider session={pageProps.session}>
       <ChakraProvider resetCSS theme={theme}>

aac792d2ac21d0fce2f4fb09d63a2a1686a9a887
esta-rossa <rajiabdou9@gmail.com>
2021-12-16 21:42:14 +0100

    feat(comment): add padding for the comment box

--- src/components/report/comment.tsx
+++ src/components/report/comment.tsx
@@ -33,60 +33,64 @@ const CommentBox = ({
   const user = session.data.user as authUser;
 
   return (
-    <Box
-      bg="white"
-      m="2"
-      ml={{ base: "10px", lg: "40px" }}
-      border="1px solid #d0d7de"
-      borderRadius="md"
-      minH="120px"
-      w={{ base: "95%", lg: "90%" }}
-    >
-      <Center>
-        <Box
-          onClick={() => editor.commands.focus()}
-          p="0.4"
-          shadow="sm"
-          border={editor.isFocused ? "1px solid #0969da" : "1px solid #d0d7de"}
-          boxShadow={
-            editor.isFocused
-              ? "rgba(9, 105, 218, 0.3) 0px 0px 0px 3px"
-              : undefined
-          }
-          minH="100px"
-          borderRadius="md"
-          bg={editor.isFocused ? "white" : "#f6f8fa"}
-          m="3"
-          w="95%"
-        >
-          {editor ? <EditorContent editor={editor} /> : <Box />}
-        </Box>
-      </Center>
-      <Flex w="95%" justify="flex-end">
-        <Button
-          onClick={() => {
-            mutation.mutate({
-              reportId: reportId,
-              body: editor.getJSON(),
-              author:
-                report.anonymous && !user.isStaff ? "anonymous" : user.login,
-              byStaff: user.isStaff,
-            });
-          }}
-          isLoading={mutation.isLoading}
-          h={{ base: "30px", md: "40px" }}
-          w={{ base: "80px", md: "100px" }}
-          fontSize={{ base: "sm", md: "md" }}
-          _hover={{}}
-          isDisabled={editor.isEmpty}
-          m={{ base: "2", md: "3" }}
-          bg="black"
-          color="white"
-          pos="relative"
-        >
-          Comment
-        </Button>
-      </Flex>
+    <Box pb="50px">
+      <Box
+        bg="white"
+        m="2"
+        ml={{ base: "10px", lg: "40px" }}
+        border="1px solid #d0d7de"
+        borderRadius="md"
+        minH="120px"
+        w={{ base: "95%", lg: "90%" }}
+      >
+        <Center>
+          <Box
+            onClick={() => editor.commands.focus()}
+            p="0.4"
+            shadow="sm"
+            border={
+              editor.isFocused ? "1px solid #0969da" : "1px solid #d0d7de"
+            }
+            boxShadow={
+              editor.isFocused
+                ? "rgba(9, 105, 218, 0.3) 0px 0px 0px 3px"
+                : undefined
+            }
+            minH="100px"
+            borderRadius="md"
+            bg={editor.isFocused ? "white" : "#f6f8fa"}
+            m="3"
+            w="95%"
+          >
+            {editor ? <EditorContent editor={editor} /> : <Box />}
+          </Box>
+        </Center>
+        <Flex w="95%" justify="flex-end">
+          <Button
+            onClick={() => {
+              mutation.mutate({
+                reportId: reportId,
+                body: editor.getJSON(),
+                author:
+                  report.anonymous && !user.isStaff ? "anonymous" : user.login,
+                byStaff: user.isStaff,
+              });
+            }}
+            isLoading={mutation.isLoading}
+            h={{ base: "30px", md: "40px" }}
+            w={{ base: "80px", md: "100px" }}
+            fontSize={{ base: "sm", md: "md" }}
+            _hover={{}}
+            isDisabled={editor.isEmpty}
+            m={{ base: "2", md: "3" }}
+            bg="black"
+            color="white"
+            pos="relative"
+          >
+            Comment
+          </Button>
+        </Flex>
+      </Box>
     </Box>
   );
 };
